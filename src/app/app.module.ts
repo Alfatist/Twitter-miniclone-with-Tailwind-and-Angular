6c6450b3123fd027622fd500bd5a6b324baac30a
MATEUS HENRIQUE OLIVEIRA DOS SANTOS <19826221724@cefet-rj.br>
2023-05-05 18:10:26 -0300

    header and some progress on new post, like profile photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NewPostComponent,
+    TweetComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/tweet/tweet.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css'],
+})
+export class TweetComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
