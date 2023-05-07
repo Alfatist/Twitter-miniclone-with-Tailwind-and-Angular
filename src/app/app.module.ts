051610fcc4d2082248594539d8508f471b5faac9
Alfatista2 <teteusoliveira9@gmail.com>
2023-05-07 00:08:45 -0300

    twitter new feed style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -18,7 +18,7 @@ import { FooterComponent } from './components/footer/footer.component';
     TweetComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent implements OnInit {
+  tweetMessage: string = '';
+  tweetMaxLength: number = 240;
+  tweetRemain: number = this.tweetMaxLength;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  sendTweet() {
+    alert('twitter created');
   }
 
+  changeTweet() {
+    this.tweetRemain = 240 - this.tweetMessage.length;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
